1556053db008982c250acdebb0af1a39f8cdc097
Tuguldur Bayarnyam <tuguldurbayarnyam@Tuguldurs-MBP.att.net>
2017-06-02 10:56:25 -0700

    test

--- src/core/postlists/action.js
+++ src/core/postlists/action.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
 import { API_BASE_URL } from '../constants';
-
 export const fetchPosts = () => {
   return axios({
     method: "GET",
-    url: `${API_BASE_URL}/wp-json/wp/v2/posts?_embed`
+    url: `${API_BASE_URL}/feed/json`
   })
     .then(res => {
       return {
         type: "FETCH_POSTS_FULFILLED",
-        payload: res.data
+        payload: res.data.items
       };
     })
     .catch(res => {
